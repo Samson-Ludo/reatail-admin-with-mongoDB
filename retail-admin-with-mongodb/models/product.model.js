1f603d5866f2557989ad7e9e39a57ed9988c8127
Samson-Ludo <43629180+Samson-Ludo@users.noreply.github.com>
2019-08-19 14:07:22 +0100

    Update product.model.js

--- retail-admin-with-mongodb/models/product.model.js
+++ retail-admin-with-mongodb/models/product.model.js
@@ -1,25 +1,19 @@
 const mongoose = require('mongoose');
 
 var productSchema = new mongoose.Schema({
-    fullName: {
+    productName: {
         type: String,
-        // required: 'This field is required.'
     },
-    email: {
+    productID: {
         type: String
     },
-    mobile: {
+    price: {
         type: String
     },
-    city: {
+    currentStock: {
         type: String
     }
 });
 
-// Custom validation for email
-// productSchema.path('email').validate((val) => {
-//     emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return emailRegex.test(val);
-// }, 'Invalid e-mail.');
 
 mongoose.model('Product', productSchema);
